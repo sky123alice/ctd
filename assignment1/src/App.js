30cbcb24a78f69f45738f6bbcb54eb87fa22cd12
unknown <alicelee.0418@gmail.com>
2021-10-19 01:55:50 -0700

    finished

--- assignment1/src/App.js
+++ assignment1/src/App.js
@@ -1,11 +1,35 @@
 import React from 'react';
 
+const todoList = [
+  {
+    id: '1',
+    title: 'watch lectures'
+  },
+  {
+    id: '2',
+    title: 'read textbook'
+  },
+  {
+    id: '3',
+    title: 'complete assignment'
+  },
+
+
+];
+
 function App() {
   return (
     
       <div>
-        <h1>Hello World</h1>
+        <h1>Todo List </h1>
+        
+        <ul>{
+          todoList.map(function (item){
+            return <li key={item.id}>{item.title}</li>;
+          })}</ul>
+        
       </div>
+   
   );
 }
 
