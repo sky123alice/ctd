b1693c9e1f2577684290d28b6a5aa27ae9bc1034
unknown <alicelee.0418@gmail.com>
2021-10-19 01:18:20 -0700

    first function component

--- assignment1/src/App.js
+++ assignment1/src/App.js
@@ -2,16 +2,10 @@ import React from 'react';
 
 function App() {
   return (
-    <div style={{ textAlign: 'center' }}>
-      <header>
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-          Learn React
-        </a>
-      </header>
-    </div>
+    
+      <div>
+        <h1>Hello World</h1>
+      </div>
   );
 }
 
